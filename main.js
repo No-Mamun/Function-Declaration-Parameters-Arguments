d5ccd434d22ee2faa038bb6137bf33ba9b321685
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-03-10 08:05:26 +0600

    Add files via upload
    Function Declaration, Parameters & Arguments

---
+++ main.js
@@ -0,0 +1,441 @@
+
+// function
+
+/*
+// No:1
+
+// creating a function
+
+function square() {
+
+              var num = 5;
+
+              var result = num * num ;
+
+document.write("result =" + result + "<br>");
+
+}
+// calling a function
+
+square();
+
+square();
+
+square();
+
+No:2
+
+// creating a function
+
+function square(num) {
+
+              var result = num * num ;
+
+document.write("result =" + result + "<br>");
+
+}
+// calling a function
+
+square(5);
+
+square(6);
+
+square(7);
+
+No:3
+
+// creating a function
+
+function square(num1, num2) {
+
+              var result = num1 * num2 ;
+
+document.write("result =" + result + "<br>");
+
+}
+// calling a function
+
+square(5,7);
+
+No:4
+
+// add function
+
+function addition (x, y){
+
+              var result = x + y;
+
+              document.write("sum =" + result + "<br>");
+}
+
+addition(10, 20);
+addition(10, 30);
+
+No:5
+
+// add function
+
+function subtraction (x, y){
+
+              var result = x - y;
+
+              document.write("sub = " + result + "<br>");
+}
+
+
+subtraction(10, 20);
+
+No:6
+
+// calling a function
+
+function square (num1,num2,num3){
+
+              var result = (num1 + num2) * num3;
+
+              document.write("square = " + result + "<br>");
+
+}
+
+// calling a function
+
+square(5,3,2);
+
+square(5,4,3);
+
+square(5,5,4);
+
+No:7
+
+Function declaration
+
+function greetings (){
+
+              console.log("Hello World");
+
+              document.write("Hello Tere" + "<br>");
+
+}
+greetings ();
+greetings ();
+greetings ();
+
+No:8
+
+Function Expression
+
+const speak = function (){
+
+              console.log("Hello World");
+
+              document.write("HELLO WORLD" + "<br>");
+
+};
+
+speak ();
+speak ();
+speak ();
+
+// No:9
+
+// parameters & Arguments
+
+const speak = function (fullName){
+
+              console.log(`Hello World ${fullName}`);
+
+              document.write("HELLO WORLD" + "<br>");
+
+};
+
+speak ('Bangladesh');
+
+speak ("Pakisthan");
+
+speak ('India');
+
+No:10
+
+// parameters & Arguments
+
+const speak = function (firstName,lastName){
+
+              console.log(`HELLO WORLD ${firstName} ${lastName}`);
+
+              document.write(`hello world ${firstName} ${lastName} "<br>"`);
+
+};
+
+speak ('Bangladesh','Mamun');
+
+speak ("Pakisthan","Moyna");
+
+speak ('India','Khan');
+
+No:11
+
+// parameters & Arguments
+
+const speak = function (time,firstName,lastName){
+
+              console.log(`HELLO WORLD ${time} ${firstName} ${lastName}`);
+
+              document.write(`hello world ${time} ${firstName} ${lastName} "<br>"`);
+
+};
+
+speak ('Morning','Bangladesh','Mamun');
+
+speak ('Noon',"Pakisthan","Moyna");
+
+speak ('Evening','India','Khan');
+
+
+No:12
+
+function sleep (){
+
+              console.log("Mamun is sleeping from 10PM");
+
+}
+
+sleep ();
+
+console.log("My name is Mamun")
+
+
+No:13
+
+let fnumber = 21;
+
+let snumber = 4;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber - getMod;
+
+console.log (devide);
+
+console.log (21 % 4);
+
+No:14
+
+let fnumber = 21;
+
+let snumber = 4;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber - getMod;
+
+console.log (getMod);
+
+console.log (21 % 4);
+
+No:15
+
+let fnumber = 21;
+
+let snumber = 4;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = getMod * snumber ;
+
+console.log (getMod);
+
+console.log (21 % 4);
+
+No:16
+
+let fnumber = 21;
+
+let snumber = 4;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = getMod * snumber ;
+
+console.log (getFinal);
+
+console.log (21 % 4);
+
+No:17
+
+let fnumber = 21;
+
+let snumber = 4;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber- getMod * snumber ;
+
+console.log (getFinal);
+
+console.log (70 % 9);
+
+ No:18
+
+let fnumber = 70;
+
+let snumber = 9;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber- getMod * snumber ;
+
+console.log (getFinal);
+
+console.log (70 % 9);
+
+console.log (21 % 5);
+
+No:19
+
+function
+
+function vagSesh () {
+
+let fnumber = 70;
+
+let snumber = 9;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber- getMod * snumber ;
+
+console.log (getFinal);
+
+}
+console.log (70 % 9);
+
+console.log (21 % 5);
+
+vagSesh ()
+
+console.log("Function executed below");
+
+
+No:20
+
+function
+
+function vagSesh (a,b) {
+
+let fnumber = a;
+
+let snumber = b;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber- getMod * snumber ;
+
+console.log (getFinal);
+
+}
+console.log("Function executed below");
+
+vagSesh (70,9)
+
+vagSesh (21,5)
+
+vagSesh (7,2)
+
+vagSesh (40,6)
+
+console.log("normal mod executed below")
+
+console.log(70 % 9)
+
+console.log(21 % 5)
+
+No:21
+
+// function
+
+function vagSesh (a,b) {
+
+let fnumber = a;
+
+let snumber = b;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber- getMod * snumber ;
+
+console.log (getFinal);
+
+return getFinal;
+
+}
+
+let mymod = vagSesh(105, 3);
+
+console.log(mymod);
+
+console.log("function executed below");
+
+vagSesh (70,9)
+
+vagSesh (21,5)
+
+vagSesh (7,2)
+
+vagSesh (40,6)
+
+No:22
+
+// function
+
+function vagSesh (a,b) {
+
+let fnumber = a;
+
+let snumber = b;
+
+let devide = fnumber / snumber;
+
+let getMod = Math.floor(devide);
+
+let getFinal = fnumber- getMod * snumber ;
+
+console.log (getFinal);
+
+}
+
+let mymod = vagSesh(107, 3);
+
+console.log("variable");
+
+console.log(mymod);
+
+console.log("function executed below");
+
+vagSesh (70,9)
+
+vagSesh (21,5)
+
+vagSesh (7,2)
+
+vagSesh (40,6)
+
+
+*/
